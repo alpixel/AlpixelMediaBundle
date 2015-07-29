bd10ff478a746d4a6a5adbff7263d3f9be099d52
Alexis BUSSIERES <alexis@alpixel.fr>
2015-07-29 14:14:42 +0200

    Add preview icon

---
+++ src/Alpixel/Bundle/MediaBundle/Twig/Extension/MediaPreviewExtension.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Alpixel\Bundle\MediaBundle\Twig\Extension;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+use Doctrine\ORM\EntityManager;
+
+class MediaPreviewExtension extends \Twig_Extension
+{
+    protected $entityManager;
+    protected $requestStack;
+    protected $previewIcons;
+
+    public function __construct(RequestStack $requestStack, EntityManager $entityManager, $previewIcons)
+    {
+        $this->requestStack  = $requestStack->getCurrentRequest();
+        $this->entityManager = $entityManager;
+        $this->previewIcon   = $previewIcons;
+    }
+
+    public function getFilters()
+    {
+        return array(
+            'previewIcon' => new \Twig_Filter_Method($this, 'previewIconFilter', array('is_safe' => array('html')))
+        );
+    }
+
+    public function previewIconFilter($secretKey = '')
+    {
+        if($secretKey == '')
+            return '';
+
+        $mimeType = $this->getMimeType($secretKey);
+
+        if(preg_match('/^image/', $mimeType) === 0) {
+            $icon = $this->getIcon($mimeType);
+            $link = $this->generatePath(true, $icon);
+            return '<img src="'.$link.'" /><div id="preview-template" style="display: none;"></div>';
+        }
+
+        $link = $this->generatePath(false, $secretKey);
+        return '<img src="'.$link.'" data-key="'.$secretKey.'" />';
+
+    }
+
+    protected function getIcon($mimeType)
+    {
+        $explMime = explode('/', $mimeType);
+        $mime     = (isset($explMime[1])) ? $explMime[1] : '';
+        return (array_key_exists($mime, $this->previewIcon)) ? $this->previewIcon[$mime] : $this->previewIcon['unknown'];
+    }
+
+    protected function generatePath($isIcon, $str)
+    {
+        if($isIcon === true) {
+            return $this->requestStack->getSchemeAndHttpHost().$this->requestStack->getBasePath().'/bundles/media/images/'.$str;
+        }
+
+        return $this->requestStack->getSchemeAndHttpHost().$this->requestStack->getBaseUrl().'/media/'.$str.'/admin';
+    }
+
+    protected function getMimeType($secretKey)
+    {
+        $mediaObject = $this->entityManager->getRepository('MediaBundle:Media')->findOneBySecretKey($secretKey);
+
+        return ($mediaObject !== null) ? $mediaObject->getMime() : null;
+    }
+
+    public function getName()
+    {
+        return 'alpixel_media_twig_media_preview_extension';
+    }
+}
