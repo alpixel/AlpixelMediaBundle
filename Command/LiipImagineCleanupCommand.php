f747b132f2fdbd3b6538eafb0efeb9ee8df6a6aa
Benjamin Hubert <benjamin@alpixel.fr>
2016-03-03 15:52:30 +0100

    Editing bad cronjob timing

--- Command/LiipImagineCleanupCommand.php
+++ Command/LiipImagineCleanupCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * @CronJob("PT1W")
+ * @CronJob("P7D")
  */
 class LiipImagineCleanupCommand extends RemoveCacheCommand
 {
