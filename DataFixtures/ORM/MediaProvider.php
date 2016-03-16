ed5835f94a6cd03f779effc4b1cca4d4a0ce51a3
Benjamin Hubert <benjamin@alpixel.fr>
2016-03-16 09:23:45 +0100

    Update MediaProvider.php

--- DataFixtures/ORM/MediaProvider.php
+++ DataFixtures/ORM/MediaProvider.php
@@ -10,7 +10,7 @@ class MediaProvider extends BaseProvider
 {
 	protected $mediaManager;
 
-	public function __construct(MediaManager $mediaManager) 
+	public function __construct(MediaManager $mediaManager)
 	{
 		$this->mediaManager = $mediaManager;
 	}
@@ -24,7 +24,7 @@ class MediaProvider extends BaseProvider
 
    protected function generateUrl ($width = null, $height = null, $type = "color")
    {
-   		$url = "http://lorempixel.com/";
+   		$url = "http://loremflickr.com/";
 
    		if ($type !== 'color') {
    			$url .= 'g/';
@@ -41,12 +41,12 @@ class MediaProvider extends BaseProvider
 
    		$url .= $width.'/'.$height;
 
-   		$category = ['abstract', 'city', 'nature'];
+   		$category = ['abstract', 'city', 'nature', 'moutains'];
    		$url .= '/'.$category[array_rand($category, 1)].'/';
    		return $url;
    }
 
-   protected function downloadMedia($url) 
+   protected function downloadMedia($url)
    {
    	$filepath = sys_get_temp_dir().'/tmp.jpg';
 	$ch = curl_init($url);
@@ -56,6 +56,6 @@ class MediaProvider extends BaseProvider
 	curl_exec($ch);
 	curl_close($ch);
 	fclose($fp);
-	return new File($filepath, 'random');	
+	return new File($filepath, 'random');
    }
 }
