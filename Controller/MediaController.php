316417db5e4c6e1a4993da42815afa211f634d96
Benjamin HUBERT <benjamin@alpixel.fr>
2016-04-05 15:18:12 +0200

    style

--- Controller/MediaController.php
+++ Controller/MediaController.php
@@ -24,7 +24,7 @@ class MediaController extends Controller
             $media = $this->get('alpixel_media.manager')->upload($file, $request->get('folder'), null);
         }
 
-        $file_uploaded = $this->get('alpixel_media.manager')->generateUrl($media);
+        $file_uploaded = $this->get('alpixel_media.manager')->generateUrl($media, ['filter' => 'admin']);
 
         return $this->render('AlpixelMediaBundle:admin:blocks/upload_wysiwyg.html.twig', [
             'file_uploaded' => $file_uploaded,
